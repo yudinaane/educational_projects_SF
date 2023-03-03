cc8830d4b2e17600dd316587aa46458f77c9bce0
yudinaane <112169806+yudinaane@users.noreply.github.com>
2023-03-03 10:43:34 +0500

    Add files via upload
    Задание 12.7.3
    
    Вам дан словарь per_cent с распределением процентных ставок по вкладам в различных банках таким образом, что ключ — название банка, значение — процент. Напишите программу, в результате которой будет сформирован список deposit значений — накопленные средства за год вклада в каждом из банков. На вход программы с клавиатуры вводится сумма money, которую человек планирует положить под проценты.
    per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
    Пример работы программы при money = 100000, deposit = [5600, 5900, 4280, 4000] .
    
    Для самостоятельного изучения вам была дана ссылка на методы для работы со списками. Изучите методы и найдите тот, который позволяет найти максимальное значение среди элементов в списке.
    
    Добавьте в программу поиск максимального значения и его вывод на экран в формате: Максимальная сумма, которую вы можете заработать — deposit[i], Где вместо deposit[i] будет выведено максимальное значение списка.

--- Yudina_12_7_3_vklad.py
+++ Yudina_12_7_3_vklad.py
@@ -1,1 +1,18 @@
 
+
+#Список банков и процентных ставок
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+per_cent_values_list=list(per_cent.values()) #Список процентных ставок
+print(per_cent_values_list)
+
+money = float(input("Введите сумму вклада:"))
+
+for i in range(len(per_cent_values_list)):
+    per_cent_values_list[i]=per_cent_values_list[i]*money/100
+    
+deposit=per_cent_values_list
+print(deposit)   #накопленные средства за год вклада в каждом из банков
+
+max_deposit=max(deposit)
+print("Максимальная сумма, которую вы можете заработать - ", max_deposit)
+
